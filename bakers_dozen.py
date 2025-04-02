a9a4b5d69d17b63aa0a839d2c1a008201565bc2c
Kokomilks <192325198+Kokomilks@users.noreply.github.com>
2025-04-02 18:15:22 +0800

    bakers dozen

--- bakers_dozen.py
+++ bakers_dozen.py
@@ -1,18 +1,18 @@
-trays = int(input("Enter number of trays: "))
+quantity_trays = int(input("Enter number of trays: "))
 
-if trays < 0:
+if quantity_trays < 0:
     print("Invalid input!")
-elif trays == 0:
+elif quantity_trays == 0:
     print("No bread")
 else:
-    total_loaves = trays * 3
+    total_bread = quantity_trays * 3
     
-    dozens = total_loaves // 13  
-    remaining_loaves = total_loaves % 13
+    dozens = total_bread // 13  
+    remaining_bread = total_bread % 13
 
-    if dozens > 0 and remaining_loaves > 0:
-        print(f"{dozens} baker's dozen{'s' if dozens > 1 else ''} and {remaining_loaves} {'loaves' if remaining_loaves > 1 else 'loaf'} of bread")
+    if dozens > 0 and remaining_bread > 0:
+        print(f"{dozens} baker's dozen{'s' if dozens > 1 else ''} and {remaining_bread} {'loaves' if remaining_bread > 1 else 'loaf'} of bread")
     elif dozens > 0:
         print(f"{dozens} baker's dozen{'s' if dozens > 1 else ''}")
     else:
-        print(f"{remaining_loaves} {'loaves' if remaining_loaves > 1 else 'loaf'} of bread")
+        print(f"{remaining_bread} {'loaves' if remaining_bread > 1 else 'loaf'} of bread")
