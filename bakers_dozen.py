0c92626516bab6d8543cae23167fccf739b24fde
Kokomilks <192325198+Kokomilks@users.noreply.github.com>
2025-04-02 18:16:47 +0800

    bakers_dozen

--- bakers_dozen.py
+++ bakers_dozen.py
@@ -7,12 +7,12 @@ elif quantity_trays == 0:
 else:
     total_bread = quantity_trays * 3
     
-    dozens = total_bread // 13  
+    dozen_bread = total_bread // 13  
     remaining_bread = total_bread % 13
 
-    if dozens > 0 and remaining_bread > 0:
-        print(f"{dozens} baker's dozen{'s' if dozens > 1 else ''} and {remaining_bread} {'loaves' if remaining_bread > 1 else 'loaf'} of bread")
-    elif dozens > 0:
-        print(f"{dozens} baker's dozen{'s' if dozens > 1 else ''}")
+    if dozen_bread > 0 and remaining_bread > 0:
+        print(f"{dozen_bread} baker's dozen{'s' if dozen_bread > 1 else ''} and {remaining_bread} {'loaves' if remaining_bread > 1 else 'loaf'} of bread")
+    elif dozen_bread > 0:
+        print(f"{dozen_bread} baker's dozen{'s' if dozen_bread > 1 else ''}")
     else:
         print(f"{remaining_bread} {'loaves' if remaining_bread > 1 else 'loaf'} of bread")
