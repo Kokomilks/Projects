1868d4e02d2458d62588091f458b96ffa1a4e9f9
Kokomilks <192325198+Kokomilks@users.noreply.github.com>
2025-04-21 21:10:04 +0800

    iteration

---
+++ iteration.py
@@ -0,0 +1,21 @@
+"""
+# 1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024 (power of 2)
+n = 1
+while n <= 1024:
+    print(n)
+    n *= 2
+
+n = 1
+while n <= 1024:
+    print(n)
+    n += n
+"""
+
+"""
+# Checks if n is divisible to 4 or 7
+n = 1
+while n <= 50:
+    if n % 4 == 0 or n % 7 == 0:
+        print(n)
+    n += 1
+"""
