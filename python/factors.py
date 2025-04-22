ce570c6c59c3af839734b73826128a148e5fd2bc
Kokomilks <192325198+Kokomilks@users.noreply.github.com>
2025-04-22 12:23:19 +0800

    revision

--- python/factors.py
+++ python/factors.py
@@ -4,7 +4,7 @@ divisor = 2
 print(f"Factors of {number} are: 1", end=' ')
 
 while divisor <= number // 2:
-    if divisor % number == 0:
+    if number % divisor == 0:
         print(divisor, end=' ')
     divisor += 1
 
