254d3a4abbfe5d59b98991237bc61316665006f2
Kokomilks <192325198+Kokomilks@users.noreply.github.com>
2025-04-22 12:20:25 +0800

    factors

--- python/factors.py
+++ python/factors.py
@@ -1,7 +1,11 @@
 number = int(input("Enter a number: "))
 
 divisor = 2
+print(f"Factors of {number} are: 1", end=' ')
 
 while divisor <= number // 2:
-    divisor % number == 0
-    print
+    if divisor % number == 0:
+        print(divisor, end=' ')
+    divisor += 1
+
+print(number)
