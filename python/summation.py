54f7508fd3cc86bb0511a93daff4d31a48c7be52
Kokomilks <192325198+Kokomilks@users.noreply.github.com>
2025-04-24 16:58:29 +0800

    summation

---
+++ python/summation.py
@@ -0,0 +1,12 @@
+import sys
+
+number = int(input("Enter a number: "))
+if number < 0:
+    sys.exit("\nInvalid Input")
+
+sum = 0
+counter = 1
+while counter <= number:
+    sum += counter
+    counter += 1
+print(f"\nSum is {sum}.")
