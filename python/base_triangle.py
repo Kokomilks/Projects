801ea6729968d136803d3dfa272a7b51de34f352
Kokomilk <192325198+Kokomilks@users.noreply.github.com>
2025-04-30 08:52:16 +0800

    Update base_triangle.py

--- python/base_triangle.py
+++ python/base_triangle.py
@@ -39,3 +39,20 @@ while row <= size:
         column += 1
     print()
     row += 1
+
+size = int(input("Enter size of base: "))
+
+print()
+row = 1
+while row <= size:
+    print(" " * (size - row), end='')
+    column = row
+    while column > 1:
+        print(column, end='')
+        column -= 1
+    column = 1
+    while column <= row:
+        print(column, end='')
+        column += 1
+    print()
+    row += 1
