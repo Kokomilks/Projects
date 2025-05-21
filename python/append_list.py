739a97f42aba9dafaa908c386e01ffd18df746f5
Kokomilk <192325198+Kokomilks@users.noreply.github.com>
2025-05-21 08:32:08 +0800

    Update append_list.py

--- python/append_list.py
+++ python/append_list.py
@@ -1,9 +1,10 @@
+# use for loop
 count = int(input("Enter number of inputs: "))
 
 print("Enter values")
 numbers = []
 counter = 1
-while counter <= count:
+for counter in range(1, count + 1):
     print(f"{counter}. ", end='')
     number = int(input())
     numbers.append(number)
@@ -19,3 +20,30 @@ print("\nOdd numbers: ", end='')
 for number in numbers:
     if number % 2 != 0:
         print(number, end=' ')
+
+# use of while loop
+count = int(input("Enter number of inputs: "))
+
+print("Enter values")
+numbers = []
+counter = 1
+for counter in range(count):
+    print(f"{counter + 1}. ", end='')
+    number = int(input())
+    numbers.append(number)
+    counter += 1
+
+print("\nEven numbers: ", end='')
+index = 0
+while index < count:
+    if numbers[index] % 2 == 0:
+        print(numbers[index], end=' ')
+    index += 1
+
+
+print("\nOdd numbers: ", end='')
+index = 0
+while index < count:
+    if numbers[index] % 2 != 0:
+        print(numbers[index], end=' ')
+    index += 1
