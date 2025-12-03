f38858c1c1f17afaae28e57ae8279d85c926543e
Kokomilk <192325198+Kokomilks@users.noreply.github.com>
2025-12-03 23:19:21 +0800

    java update

--- java_program/TestProgram.java
+++ java_program/TestProgram.java
@@ -1,9 +1,11 @@
+package java_program;
+
 public class TestProgram {
     
     public static void clear() {
     System.out.print("\033[H\033[2J");
     System.out.flush();
-}
+    }
     public static void main(String[] args) {
         clear();
         System.out.println("Test String");
