084d271adfd5c319560978dad6b5e88fee411b7c
Kokomilk <192325198+Kokomilks@users.noreply.github.com>
2025-12-04 01:16:29 +0800

    finally!

--- java_program/RussianPeasantAlgorithm.java
+++ java_program/RussianPeasantAlgorithm.java
@@ -6,21 +6,25 @@ public class RussianPeasantAlgorithm {
     System.out.print("\033[H\033[2J");
     System.out.flush();
     }
-
     static Scanner scanner = new Scanner(System.in);
+    public static int algorithm() {
+        System.out.print("Enter a multiplicand: ");
+        int multiplicand = scanner.nextInt();
+        System.out.print("Enter a multiplier: ");
+        int multiplier = scanner.nextInt();
+
+        int final_multiplier = 0;
+        while(multiplicand > 0) {
+            if(multiplicand % 2 != 0) {
+                final_multiplier += multiplier;
+            }
+            multiplicand /= 2;
+            multiplier *= 2;
+        }
+        return final_multiplier;
+    }
     public static void main(String[] args) {
         clear();
-        int multiplicand;
-        System.out.print("Enter a multiplicand: ");
-        multiplicand = scanner.nextInt();
-
-        /*int multiplier;
-        System.out.println("Enter a multiplier: ");
-        multiplier = scanner.nextInt();*/
-        
-        for(; multiplicand != 1;) {
-            multiplicand = (int) Math.floor(multiplicand / 2);
-            System.out.println(multiplicand);
-        }
+        System.out.println(algorithm());
     }
 }
