e094069236b1bae7bd9847557c33c430f06c826a
Kokomilk <192325198+Kokomilks@users.noreply.github.com>
2025-12-05 18:54:17 +0800

    areaRectangle

---
+++ java_program/areaRectangle.java
@@ -0,0 +1,9 @@
+package java_program;
+
+public class areaRectangle {
+    public static void main(String[] args) {
+        int length = 5, width = 8;
+        int area = length * width;
+        System.out.println(area);
+    }
+}
