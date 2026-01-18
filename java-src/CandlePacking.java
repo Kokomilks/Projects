dc1c539d11496ffe48a9db7463cb9b529c04b4a0
Kokomilks <192325198+Kokomilks@users.noreply.github.com>
2026-01-18 22:44:41 +0800

    update CandlePacking.java

--- java-src/CandlePacking.java
+++ java-src/CandlePacking.java
@@ -15,5 +15,16 @@ public class CandlePacking {
                 System.out.println("Input error, diameter input must be 1 or 2 only.");
                 System.exit(0); 
             }
+        int oneDimension = 0;
+            try {
+                System.out.print("Enter dimension one value: ");
+                oneDimension = scanner.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input, dimension one input must not be letters or special symbols.");
+            }
+            if (oneDimension < 1) {
+                System.out.println("Input error, dimension one input must not be less than 1.");
+                System.exit(0);
+            }
     }
 }
